302b8933043a2fa9cf55a72d2a20be50c9c00aed
Bestimran <imrantahir9918@gmail.com>
2022-08-22 13:37:01 -0700

    calculate btn added

--- js/budget.js
+++ js/budget.js
@@ -10,7 +10,7 @@ const TotalPlayerValue = parseFloat(TotalPlayerString);
 TotalPlayerValue.innerText = TotalPlayerValue;
 
 const TotalPlayerCost = inputFieldValue * TotalPlayerValue;
-// console.log(TotalPlayerCost);
+
 
 const perPlayerCost = document.getElementById('per-Player');
 const perPlayerString = perPlayerCost.innerText;
@@ -20,6 +20,12 @@ perPlayerCost.innerText = previousValue;
 const TotalExpense = TotalPlayerCost + previousValue;
 perPlayerCost.innerText = TotalExpense;
 
+})
 
-
+document.getElementById('btn-calculate').addEventListener('click', function(){
+    const inputManager = document.getElementById('input-manager');
+    const inputManagerString = inputManager.value;
+    const inputValue = parseFloat(inputManagerString);
+    inputManager.value = inputValue;
+  
 })
